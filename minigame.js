5828e47e576bc4bd0ff7a26eef5ab6f6b76f1662
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2020-03-25 09:21:25 -0600

    minigame.js mostly complete. Need to debug an endless loop error.

--- minigame.js
+++ minigame.js
@@ -9,20 +9,26 @@ while (activePlayers.length !== 1) {
     for (let i = 0; i < switchcount; i++) {
         activeswitches.push(i)
     }
-    console.log(activeswitches)
+    console.log(`The active switches are ${activeswitches}.`)
     while (bombswitchdetonated === false) {
         let currentplayer = activePlayers[0]
-        let switchpicked = Math.floor(Math.random() * switchcount)
+        let switchpicked = Math.floor(Math.random() * activeswitches.length)
+        console.log(`Player ${currentplayer} picked switch number ${switchpicked}.`)
         if (switchpicked === bombswitch) {
-            //remove currentPlayer from active players
-            //restart with 3 players
+            console.log(`Player ${currentplayer} detonated the bomb!`)
+            activePlayers = activePlayers.filter(player => player !== currentplayer)
             bombswitchdetonated = true;
         }
         else {
+            console.log('The switch was a dud!')
             //rotate currentplayer to back of line
+            activePlayers = activePlayers.filter(player => player !== currentplayer)
+            activePlayers.push(currentplayer)
             //remove switch picked from active switches
+            activeswitches = activeswitches.filter(s => s !== switchpicked)
             if (activeswitches.length === 1) {
                 bombswitchdetonated = true; //Not actually detonated, but used to reset the arena, since the only switch left would be the bombswitch. This allows the game to reset the active switch count;
+                console.log('The switches have been reset!')
             }
         }
     }
