b4c3c3147ea27fb679b913d6b121dc478cb4e1c4
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2020-03-25 09:59:19 -0600

    simulation complete

--- app.js
+++ app.js
@@ -2,10 +2,34 @@
 const minigame = require("./minigame")
 
 function simulate(x) {
+    console.log('Program has started...please wait...')
+    let p1WinCount = 0;
+    let p2WinCount = 0;
+    let p3WinCount = 0;
+    let p4WinCount = 0;
     for (let i = 0; i < x; i++) {
         let res = minigame()
-        console.log(`Player ${res} won!`)
+        if (res === 1) {
+            p1WinCount++
+        }
+        else if (res === 2) {
+            p2WinCount++
+        }
+        else if (res === 3) {
+            p3WinCount++
+        }
+        else if (res === 4) {
+            p4WinCount++
+        }
+        else {
+            throw new error("Something broke. The player that won wasn't even playing. This line of code should never execute.")
+        }
     }
+    console.log(`The minigame was played a total of ${x} time(s).`)
+    console.log(`Player 1 won ${p1WinCount} time(s). That's a ${((p1WinCount / x) * 100).toPrecision(4)}% winrate!`)
+    console.log(`Player 2 won ${p2WinCount} time(s). That's a ${((p2WinCount / x) * 100).toPrecision(4)}% winrate!`)
+    console.log(`Player 3 won ${p3WinCount} time(s). That's a ${((p3WinCount / x) * 100).toPrecision(4)}% winrate!`)
+    console.log(`Player 4 won ${p4WinCount} time(s). That's a ${((p4WinCount / x) * 100).toPrecision(4)}% winrate!`)
 }
 
-simulate (100000);
+simulate(100000);
